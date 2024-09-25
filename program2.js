3f25be7cad76e12ba41c656b54107cceabdbdaf4
Prachiyadav6265 <159807017+Prachiyadav6265@users.noreply.github.com>
2024-09-25 19:19:51 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,25 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+   const romanValues = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
+        let total = 0;
+        for (let i = 0; i < s.length; i++) {
+            let current = romanValues[s[i]];
+            let next = romanValues[s[i + 1]];
+            total += (next && current < next) ? -current : current;
+        }
+        
+        return total;
+    }
+    let s = "III";
+    console.log(romanToInt(s));   
 };
-
-
 module.exports={romanToInt}
